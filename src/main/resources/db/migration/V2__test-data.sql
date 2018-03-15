b00554247cc614b65a8e78cbfa3541aff9c38a56
Derrik Curran <derrikcurran@gmail.com>
2018-03-15 15:42:07 -0400

    Add an employee to DB test data

--- src/main/resources/db/migration/V2__test-data.sql
+++ src/main/resources/db/migration/V2__test-data.sql
@@ -1,1 +1,17 @@
-INSERT INTO users(username, password) VALUES('admin', '$2a$10$rXlKlM2376q.q4jxc/q/ieuBT3jYg8on88I.d6RcHF3KbAFvIFl9i');
+# Users
+
+INSERT INTO users(username, password) VALUES(
+  'admin',
+  '$2a$10$rXlKlM2376q.q4jxc/q/ieuBT3jYg8on88I.d6RcHF3KbAFvIFl9i'
+);
+
+# Employees
+
+INSERT INTO employees(first_name, middle_initial, last_name, date_of_birth, date_of_employment, status) VALUES(
+  'Jane',
+  'J',
+  'Doe',
+  '1988-06-20',
+  '2016-02-15',
+  'ACTIVE'
+);
